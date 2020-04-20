c6c30fe4296468661e6d002f3228cc466ba35773
coderider004 <59221702+coderider004@users.noreply.github.com>
2020-04-21 02:30:06 +0700

    Updates

---
+++ precache-manifest.461960ed0834c1793c6a36db766868cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3791e6a8f90b5d74d1d9f6cd29c22d1",
+    "url": "/cal-app/index.html"
+  },
+  {
+    "revision": "69785bf3deb9fc8ac852",
+    "url": "/cal-app/static/css/2.e051ca46.chunk.css"
+  },
+  {
+    "revision": "d538a9f6eecddda2a1c1",
+    "url": "/cal-app/static/css/main.69b36ea3.chunk.css"
+  },
+  {
+    "revision": "69785bf3deb9fc8ac852",
+    "url": "/cal-app/static/js/2.81d7d23f.chunk.js"
+  },
+  {
+    "revision": "33c02c42ab7314be00ddaa44dbd96c39",
+    "url": "/cal-app/static/js/2.81d7d23f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d538a9f6eecddda2a1c1",
+    "url": "/cal-app/static/js/main.830031cc.chunk.js"
+  },
+  {
+    "revision": "8dab0498368385d6bcc8",
+    "url": "/cal-app/static/js/runtime-main.0e353f64.js"
+  }
+]);
